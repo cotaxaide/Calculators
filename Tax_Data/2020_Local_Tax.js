0e45ad11462038257caad2196a13521902c47e6f
cotaxaide <43553241+cotaxaide@users.noreply.github.com>
2020-07-05 17:21:41 -0600

    Added link to 2020 data files

---
+++ Tax_Data/2020_Local_Tax.js
@@ -0,0 +1,329 @@
+//County_Tax[INDEX] = DATA;
+//City+Tax[INDEX] = DATA;
+//	INDEX = City ":" Year
+//	DATA = County tax rate ":" IRS Code ":" Other taxes
+//	Updated DR 1002 6/30/20
+State_Tax['2020'] = '2.9';
+County_Tax['Adams:2020'] = '0.75::RTD=1:CD=0.1';
+County_Tax['Alamosa:2020'] = '3:';
+County_Tax['Arapahoe:2020'] = '0.25::RTD=1:CD=0.1';
+County_Tax['Archuleta:2020'] = '4:';
+County_Tax['Baca:2020'] = '0:';
+County_Tax['Bent:2020'] = '1:';
+County_Tax['Boulder:2020'] = '0.985::RTD=1:CD=0.1';
+County_Tax['Broomfield:2020'] = '0::CD=0.1'; // 2019 change
+County_Tax['Chaffee:2020'] = '2.75:'; // 2019 change
+County_Tax['Cheyenne:2020'] = '0:';
+County_Tax['Clear Creek:2020'] = '1.65:'; // 2018 change
+County_Tax['Conejos:2020'] = '0:';
+County_Tax['Costilla:2020'] = '1:';
+County_Tax['Crowley:2020'] = '2:';
+County_Tax['Custer:2020'] = '2:';
+County_Tax['Delta:2020'] = '2:';
+County_Tax['Denver:2020'] = '0::RTD=1:'; // 2019 change
+County_Tax['Dolores:2020'] = '0:';
+County_Tax['Douglas:2020'] = '1::RTD=1:CD=0.1:';
+County_Tax['Eagle:2020'] = '1::MTS=0.5'; // 2020 change
+County_Tax['El Paso:2020'] = '1.23::RTA:1';
+County_Tax['Elbert:2020'] = '1::';
+County_Tax['Fremont:2020'] = '2.5:';
+County_Tax['Garfield:2020'] = '1:';
+County_Tax['Gilpin:2020'] = '0:';
+County_Tax['Grand:2020'] = '1.3:'; // 2017 change
+County_Tax['Gunnison:2020'] = '1::RTA=1';
+County_Tax['Hinsdale:2020'] = '5:';
+County_Tax['Huerfano:2020'] = '2:';
+County_Tax['Jackson:2020'] = '4:';
+County_Tax['Jefferson:2020'] = '0.5:B:RTD=1:CD=0.1';
+County_Tax['Kiowa:2020'] = '0:';
+County_Tax['Kit Carson:2020'] = '0:';
+County_Tax['La Plata:2020'] = '2:';
+County_Tax['Lake:2020'] = '4:';
+County_Tax['Larimer:2020'] = '0.8::'; // 2019 change
+County_Tax['Las Animas:2020'] = '1.5:'; // 2018 change
+County_Tax['Lincoln:2020'] = '2:';
+County_Tax['Logan:2020'] = '1:';
+County_Tax['Mesa:2020'] = '2::PSI=0.37';
+County_Tax['Mineral:2020'] = '2.6:';
+County_Tax['Moffat:2020'] = '2:';
+County_Tax['Montezuma:2020'] = '0::HSD=0.4';
+County_Tax['Montrose:2020'] = '1::PSI=0.75';
+County_Tax['Morgan:2020'] = '0:';
+County_Tax['Otero:2020'] = '1:';
+County_Tax['Ouray:2020'] = '2:';
+County_Tax['Park:2020'] = '1:';
+County_Tax['Phillips:2020'] = '1:';
+County_Tax['Pitkin:2020'] = '3.1::RTA=0.4:MTS=0.5';
+County_Tax['Prowers:2020'] = '1:';
+County_Tax['Pueblo:2020'] = '1::';
+County_Tax['Rio Blanco:2020'] = '3.6:';
+County_Tax['Rio Grande:2020'] = '2.6:';
+County_Tax['Routt:2020'] = '1:';
+County_Tax['Saguache:2020'] = '2.5:'; // 2019 change
+County_Tax['San Juan:2020'] = '6.5:'; // 2018 change
+County_Tax['San Miguel:2020'] = '1::RTA=0.25:'; // 2019 change
+County_Tax['Sedgwick:2020'] = '3:'; // 2020 change
+County_Tax['Summit:2020'] = '2::MHA=0.725:MTS=0.75';
+County_Tax['Teller:2020'] = '1:';
+County_Tax['Washington:2020'] = '1.5:';
+County_Tax['Weld:2020'] = '0:';
+County_Tax['Yuma:2020'] = '0:';
+
+City_Tax['Aguilar:2020'] = '3::::';
+City_Tax['Akron:2020'] = '2.5::::';
+City_Tax['Alamosa:2020'] = '2.5::::'; // 2020 change
+City_Tax['Alma:2020'] = '3::::';
+City_Tax['Antonito:2020'] = '4::::';
+City_Tax['Arrowhead:2020'] = '0::MDT=05::'; // 2020 add
+City_Tax['Arvada/Adams:2020'] = '3.46:B::';
+City_Tax['Arvada/Jefferson:2020'] = '3.46:B::';
+City_Tax['Aspen:2020'] = '2.4::RTA=0.4::'; // 2019 change
+City_Tax['Aspen Park:2020'] = '0::MDT=0.5:AP=0.25:'; // AP?
+City_Tax['Ault:2020'] = '3::::';
+City_Tax['Aurora/Adams:2020'] = '3.75:::';
+City_Tax['Aurora/Arapahoe:2020'] = '3.75:::';
+City_Tax['Aurora/Douglas:2020'] = '3.75:::::';
+City_Tax['Avon:2020'] = '4::::';
+City_Tax['Bachelor Gulch:2020'] = '0::MDT=5::'; // Added 2018
+City_Tax['Basalt/Eagle:2020'] = '3::RTA=0.8::';
+City_Tax['Basalt/Pitkin:2020'] = '3::RTA=0.8::';
+City_Tax['Bayfield:2020'] = '3::::';
+City_Tax['Bennett/Adams:2020'] = '4::::';
+City_Tax['Bennett/Arapahoe:2020'] = '4::::';
+City_Tax['Berthoud/Larimer:2020'] = '4::::'; // 2019 change
+City_Tax['Berthoud/Weld:2020'] = '4::::'; // 2019 change
+City_Tax['Black Hawk:2020'] = '6::::'; // 2017 change
+City_Tax['Blanca:2020'] = '3::::';
+City_Tax['Blue River:2020'] = '2.5::MTS=0.75::';
+City_Tax['Boulder:2020'] = '3.86:B::';
+City_Tax['Breckenridge:2020'] = '2.5::MTS=0.75::';
+City_Tax['Brighton/Adams:2020'] = '3.75::::';
+City_Tax['Brighton/Weld:2020'] = '3.75::::';
+City_Tax['Broomfield:2020'] = '4.15::RTD=1:CD=0.1::';
+City_Tax['Brush:2020'] = '4::::';
+City_Tax['Buena Vista:2020'] = '2.5::::';
+City_Tax['Burlington:2020'] = '2::::';
+City_Tax['Calhan:2020'] = '3::RTA=0::';
+City_Tax['Canon City:2020'] = '3::::'; // 2017 change
+City_Tax['Carbondale:2020'] = '3.5::RTA=1::';
+City_Tax['Castle Pines:2020'] = '2.75::::';
+City_Tax['Castle Rock:2020'] = '4::RTD=0:CD=0:';
+City_Tax['Cedaredge:2020'] = '2::::';
+City_Tax['Centennial:2020'] = '2.5:A:::';
+City_Tax['Center/Rio Grande:2020'] = '4::::'; // 2020 change
+City_Tax['Center/Saguache:2020'] = '4::::'; // 2020 change
+City_Tax['Central City:2020'] = '6::::'; // 2019 change
+City_Tax['Cherry Hills Village:2020'] = '3.5::::';
+City_Tax['Cheyenne Wells:2020'] = '2::::';
+City_Tax['Collbran:2020'] = '2::::';
+City_Tax['Colorado Springs:2020'] = '3.12::::';
+City_Tax['Columbine Valley:2020'] = '3::::';
+City_Tax['Commerce City:2020'] = '4.5::::';
+City_Tax['Cortez:2020'] = '4.05::::';
+City_Tax['Craig:2020'] = '2.25::::';
+City_Tax['Crawford:2020'] = '2::::';
+City_Tax['Creede:2020'] = '4::::'; // 2020 change
+City_Tax['Crested Butte:2020'] = '4.5::::';
+City_Tax['Crestone:2020'] = '3.5::::'; // 2020 change
+City_Tax['Cripple Creek:2020'] = '2.3::::';
+City_Tax['Dacono:2020'] = '3::::';
+City_Tax['De Beque:2020'] = '2::::';
+City_Tax['Del Norte:2020'] = '2::::';
+City_Tax['Delta:2020'] = '3::::';
+City_Tax['Denver:2020'] = '4.31::RTD=1:CD=0.1:'; // 2019 change
+City_Tax['Dillon:2020'] = '2.5::MTS=0.75::';
+City_Tax['Dinosaur:2020'] = '2.1::::';
+City_Tax['Dolores:2020'] = '3.5::::';
+City_Tax['Dove Creek:2020'] = '2::::';
+City_Tax['Durango:2020'] = '3.5::::'; // 2019 change
+City_Tax['Eads:2020'] = '2::::';
+City_Tax['Eagle:2020'] = '4.5::::';
+City_Tax['Eaton:2020'] = '3::::';
+City_Tax['Edgewater:2020'] = '3.5::::';
+City_Tax['Edwards:2020'] = '0::MDT=1::';
+City_Tax['Elizabeth:2020'] = '4::::';
+City_Tax['Empire:2020'] = '5::::';
+City_Tax['Englewood:2020'] = '3.5::::';
+City_Tax['Erie/Boulder:2020'] = '3.5::::';
+City_Tax['Erie/Weld:2020'] = '3.5::RTD=1::';
+City_Tax['Estes Park:2020'] = '5::::';
+City_Tax['Evans:2020'] = '3.5::::';
+City_Tax['Fairplay:2020'] = '4::::';
+City_Tax['Federal Heights:2020'] = '4::::';
+City_Tax['Firestone:2020'] = '3.6::::'; // 2018 change
+City_Tax['Flagler:2020'] = '2::::';
+City_Tax['Fleming:2020'] = '2::::';
+City_Tax['Florence:2020'] = '2.5::::';
+City_Tax['Fort Collins:2020'] = '3.85:B::';
+City_Tax['Fort Lupton:2020'] = '4::::';
+City_Tax['Fort Morgan:2020'] = '4::::'; // 2018 change
+City_Tax['Fountain:2020'] = '3.75::RTA=0::';
+City_Tax['Fowler:2020'] = '3::::';
+City_Tax['Foxfield:2020'] = '3.75::::';
+City_Tax['Fraser:2020'] = '5::::';
+City_Tax['Frederick:2020'] = '2.5::::';
+City_Tax['Frisco:2020'] = '2::MTS=0.75::';
+City_Tax['Fruita:2020'] = '3::::';
+City_Tax['Garden City:2020'] = '3::::';
+City_Tax['Georgetown:2020'] = '4.5::::'; // 2019 change
+City_Tax['Gilcrest:2020'] = '4::::';
+City_Tax['Glendale:2020'] = '3.75::::';
+City_Tax['Glenwood Springs:2020'] = '3.7::RTA=1::';
+City_Tax['Golden:2020'] = '3::::';
+City_Tax['Granada:2020'] = '2::::';
+City_Tax['Granby:2020'] = '4::::';
+City_Tax['Grand Junction:2020'] = '2.75::::';
+City_Tax['Grand Lake:2020'] = '5::::'; // 2017 change
+City_Tax['Greeley:2020'] = '4.11:B::';
+City_Tax['Green Mt Falls/El Paso:2020'] = '3::::';
+City_Tax['Green Mt Falls/Teller:2020'] = '3::::';
+City_Tax['Greenwood Village:2020'] = '3::::';
+City_Tax['Gunnison:2020'] = '4::RTA=0.35::';
+City_Tax['Gypsum:2020'] = '3::::';
+City_Tax['Haxtun:2020'] = '3.5::::'; // 2018 change
+City_Tax['Hayden:2020'] = '4::::';
+City_Tax['Holly:2020'] = '3::::';
+City_Tax['Holyoke:2020'] = '1.5::::';
+City_Tax['Hooper:2020'] = '2::::';
+City_Tax['Hot Sulphur Springs:2020'] = '4::::';
+City_Tax['Hotchkiss:2020'] = '2::::';
+City_Tax['Hudson:2020'] = '4::::';
+City_Tax['Hugo:2020'] = '2::::';
+City_Tax['Idaho Springs:2020'] = '4::::';
+City_Tax['Ignacio:2020'] = '2::::';
+City_Tax['Johnstown/Larimer:2020'] = '3.5::::'; // 2020 change
+City_Tax['Johnstown/Weld:2020'] = '3.5::::'; // 2020 change
+City_Tax['Julesburg:2020'] = '1::::';
+City_Tax['Keenesburg:2020'] = '3::::';
+City_Tax['Kersey:2020'] = '3.6::::';
+City_Tax['Kiowa:2020'] = '1.5::::';
+City_Tax['Kit Carson:2020'] = '2::::';
+City_Tax['Kremmling:2020'] = '4::::';
+City_Tax['La Jara:2020'] = '3::::';
+City_Tax['La Junta:2020'] = '3::HSD=1::'; // HSD added 2018
+City_Tax['La Salle:2020'] = '3.5::::';
+City_Tax['La Veta:2020'] = '3.5::::';
+City_Tax['Lafayette:2020'] = '3.5::::';
+City_Tax['Lakeside:2020'] = '3.6::::'; // 2020 change
+City_Tax['Lakewood:2020'] = '3:A::';
+City_Tax['Lamar:2020'] = '3::::';
+City_Tax['Larkspur:2020'] = '4::CD=0::';
+City_Tax['Las Animas:2020'] = '3::::';
+City_Tax['Limon:2020'] = '2::::';
+City_Tax['Littleton/Arapahoe:2020'] = '3::::';
+City_Tax['Littleton/Douglas:2020'] = '3::::';
+City_Tax['Littleton/Jefferson:2020'] = '3:B:::';
+City_Tax['Lochbuie/Adams:2020'] = '4::::';
+City_Tax['Lochbuie/Weld:2020'] = '4::::';
+City_Tax['Log Lane Village:2020'] = '3::::';
+City_Tax['Lone Tree:2020'] = '1.8125::RTD=1::';
+City_Tax['Longmont/Boulder:2020'] = '3.53:B::'; // 2018 change
+City_Tax['Longmont/Larimer:2020'] = '3.53:B:::'; // 2018 change
+City_Tax['Longmont/Weld:2020'] = '3.53:B:::'; // 2018 change
+City_Tax['Louisville:2020'] = '3.65::::'; // 2018 change
+City_Tax['Loveland:2020'] = '3::::';
+City_Tax['Lyons:2020'] = '3.5::::';
+City_Tax['Manassa:2020'] = '1::::';
+City_Tax['Mancos:2020'] = '4::::';
+City_Tax['Manitou Springs:2020'] = '3.9::::'; // 2019 change
+City_Tax['Manzanola/Crowley:2020'] = '2::::';
+City_Tax['Manzanola/Otero:2020'] = '2::::';
+City_Tax['Marble:2020'] = '2::RTA=0::';
+City_Tax['Mead:2020'] = '2::::';
+City_Tax['Milliken:2020'] = '2.5::::';
+City_Tax['Minturn:2020'] = '4::::'; // 2019 change
+City_Tax['Moffat:2020'] = '2::::';
+City_Tax['Monte Vista:2020'] = '2::::';
+City_Tax['Montezuma:2020'] = '2::::';
+City_Tax['Montrose:2020'] = '3.3::::';
+City_Tax['Monument:2020'] = '3::RTA=0::';
+City_Tax['Monument-Baptist Rd:2020'] = '3::RTA=1::';
+City_Tax['Morrison:2020'] = '3.75::::';
+City_Tax['Mountain View:2020'] = '4::::';
+City_Tax['Mountain Village:2020'] = '4.5::RTA=0.25::'; // 2019 change
+City_Tax['Mt. Crested Butte:2020'] = '5::::';
+City_Tax['Mt. Vernon CC:2020'] = '0::MDT=2::'; // Added 2018
+City_Tax['Naturita:2020'] = '4::::';
+City_Tax['Nederland:2020'] = '3.75::::';
+City_Tax['New Castle:2020'] = '3.5::RTA=0.8::';
+City_Tax['Northglenn/Adams:2020'] = '4::::';
+City_Tax['Northglenn/Weld:2020'] = '4::::';
+City_Tax['Norwood:2020'] = '3::::';
+City_Tax['Nucla:2020'] = '4::::';
+City_Tax['Nunn:2020'] = '2::::';
+City_Tax['Oak Creek:2020'] = '3::::';
+City_Tax['Ohio:2020'] = '0::RTA=0::';
+City_Tax['Olathe:2020'] = '4::::';
+City_Tax['Ordway:2020'] = '2::::';
+City_Tax['Otis:2020'] = '2::::';
+City_Tax['Ouray:2020'] = '4::::';
+City_Tax['Ovid:2020'] = '1::::';
+City_Tax['Palisade:2020'] = '2::::';
+City_Tax['Palmer Lake:2020'] = '3::RTA=0::';
+City_Tax['Paonia:2020'] = '2::::';
+City_Tax['Parachute:2020'] = '3.75::::';
+City_Tax['Parker:2020'] = '3::::';
+City_Tax['Pierce:2020'] = '2::::';
+City_Tax['Pitkin:2020'] = '3::RTA=0::';
+City_Tax['Platteville:2020'] = '3::::';
+City_Tax['Poncha Springs:2020'] = '2::::';
+City_Tax['Pueblo:2020'] = '3.7::::'; // 2018 change
+City_Tax['Red Cliff:2020'] = '3::::';
+City_Tax['Red Sky Ranch:2020'] = '0::MDT=5.5::'; // added 2018
+City_Tax['Rico:2020'] = '5::RTA=0.25::'; // Added 2020
+City_Tax['Ridgway:2020'] = '3.6::::';
+City_Tax['Rifle:2020'] = '4.25::::';
+City_Tax['Rocky Ford:2020'] = '4::::'; // 2017 change, 2018 change
+City_Tax['Romeo:2020'] = '1::::';
+City_Tax['Saguache:2020'] = '4::::';
+City_Tax['Salida:2020'] = '3::::';
+City_Tax['San Luis:2020'] = '3::::';
+City_Tax['Sawpit:2020'] = '3::::';
+City_Tax['Sedgwick:2020'] = '1::::';
+City_Tax['Seibert:2020'] = '2::::';
+City_Tax['Severance:2020'] = '3::::';
+City_Tax['Sheridan:2020'] = '3.5::::';
+City_Tax['Silt:2020'] = '3::::';
+City_Tax['Silver Cliff:2020'] = '2::::';
+City_Tax['Silver Plume:2020'] = '3::::';
+City_Tax['Silverthorne:2020'] = '2::::';
+City_Tax['Silverton:2020'] = '1::::';
+City_Tax['Simla:2020'] = '4::::';
+City_Tax['Snowmass Village:2020'] = '3.5::RTA=0.4::';
+City_Tax['Somerset:2020'] = '0::RTA=0::';
+City_Tax['South Fork/Mineral:2020'] = '2::::';
+City_Tax['South Fork/Rio Grande:2020'] = '2::::';
+City_Tax['South Park:2020'] = '0::HSD=1:'; // Added 2018
+City_Tax['Springfield:2020'] = '2::::';
+City_Tax['Steamboat Springs:2020'] = '4.5::::'; // 2017 change
+City_Tax['Sterling:2020'] = '3::RTA=0.1::';
+City_Tax['Stratton:2020'] = '2::::';
+City_Tax['Superior/Boulder:2020'] = '3.46::::';
+City_Tax['Superior/Jefferson:2020'] = '3.46::::';
+City_Tax['Telluride:2020'] = '4.5::RTA=0.25::'; // 2019 change
+City_Tax['Thornton:2020'] = '3.75:B::';
+City_Tax['Timnath:2020'] = '3::::';
+City_Tax['Trinidad:2020'] = '4::::';
+City_Tax['Two Rivers:2020'] = '0::MDT=4::';
+City_Tax['Vail:2020'] = '4::::';
+City_Tax['Victor:2020'] = '3::::';
+City_Tax['Walden:2020'] = '1::::';
+City_Tax['Walsenburg:2020'] = '3::::';
+City_Tax['Walsh:2020'] = '3::::';
+City_Tax['Ward:2020'] = '2::::';
+City_Tax['Wellington:2020'] = '3::::';
+City_Tax['Westcliffe:2020'] = '3::::'; // 2020 change
+City_Tax['Westminster/Adams:2020'] = '3.85:B::';
+City_Tax['Westminster/Jefferson:2020'] = '3.85:B::';
+City_Tax['Wheat Ridge:2020'] = '3.5::::'; // 2017 change
+City_Tax['Wiley:2020'] = '2::::';
+City_Tax['Wiggins:2020'] = '2::::';
+City_Tax['Windsor/Larimer:2020'] = '3.95::::';
+City_Tax['Windsor/Weld:2020'] = '3.95::::';
+City_Tax['Winter Park:2020'] = '7::::';
+City_Tax['Woodland Park/El Paso:2020'] = '4.09::::';
+City_Tax['Woodland Park/Teller:2020'] = '4.09::::';
+City_Tax['Wray:2020'] = '2.5::::';
+City_Tax['Yampa:2020'] = '2::::';
+City_Tax['Yuma:2020'] = '3::::';
+City_Tax['SE Jefferson County:2020'] = '0:B:SE=0.5::';
